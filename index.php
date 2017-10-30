4c5fab117972ce810b176e201106250f4346efd0
JADENLAPTOP\Jaden <emailtojaden@gmail.com>
2017-10-30 16:36:59 -0400

    Changed to php to deploy to Heroku

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>LOGAN</title>
+    <link rel="icon" type="image/ico" href="images/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="styles/normalize.css">
+    <link rel="stylesheet" type="text/css" href="styles/main.css">
+    <link rel="stylesheet" type="text/css" href="styles/index.css">
+  </head>
+  <body>
+    <header>
+      <img id="logo_img" src="images/logo.png" alt="Logan Logo">
+    </header>
+    <nav id="nav_menu">
+        <ul>
+          <li><a href="index.php" class="current">Home</a></li>
+          <li><a href="pages/about.html">ABOUT</a></li>
+          <li><a href="pages/stars.html">STARS</a></li>
+          <li><a href="pages/media.html">MEDIA</a>
+              <ul>
+                <li><a href="pages/media.html">POSTERS</a></li>
+                <li><a href="pages/photos.html">PHOTOS</a></li>
+                <li><a href="pages/videos1.html">VIDEOS</a>
+                  <ul>
+                    <li><a href="pages/videos1.html">Official Trailer 1[HD]</a></li>
+                    <li><a href="pages/videos2.html">Official Trailer 2[HD]</a></li>
+                    <li><a href="pages/videos3.html">Sunseeker</a></li>
+                  </ul>
+                </li>
+              </ul>
+          </li>
+          <li><a href="pages/feedback.html">FEEDBACK</a></li>
+        </ul>
+    </nav>
+    <aside id="asideA">
+        <br>
+        <a href="index.php"><div class="grow_thumb pic_thumb"><img src="thumbnails\nav_home.png" alt="home"></div>
+        <h2>HOME</h2></a>
+        <a href="pages/about.html"><div class="grow_thumb pic_thumb"><img src="thumbnails\nav_about.png" alt="about"></div>
+        <h2>ABOUT</h2></a>
+        <a href="pages/stars.html"><div class="grow_thumb pic_thumb"><img src="thumbnails\nav_stars.png" alt="stars"></div>
+        <h2>STARS</h2></a>
+        <a href="pages/media.html"><div class="grow_thumb pic_thumb"><img src="thumbnails\nav_media.png" alt="media"></div>
+        <h2>MEDIA</h2></a>
+        <a href="pages/feedback.html"><div class="grow_thumb pic_thumb"><img src="thumbnails\nav_feedback.png" alt="feedback"></div>
+        <h2>FEEDBACK</h2></a>
+    </aside>
+    <main>
+      <article>
+        <h1>LOGAN</h1>
+        <p>18A | 2h 21min | Action, Drama, Sci-Fi | 3 March 2017 (Canada)</p><br><br>
+        <div class="grow pic"><a href="pages/photos.html"><img src="images\ss1.jpg" alt="Screen Shot 1"></a></div>
+        <div class="grow pic"><a href="pages/photos.html"><img src="images\ss2.jpg" alt="Screen Shot 2"></a></div>
+        <div class="grow pic"><a href="pages/photos.html"><img src="images\ss3.jpg" alt="Screen Shot 3"></a></div>
+        <div class="staff">
+          <table>
+            <tr>
+              <td><h2>GENRES</h2></td>
+              <td><p>Action / Adventure</p></td>
+            </tr>
+            <tr>
+              <td><h2>DIRECTED BY</h2></td>
+              <td><p>James Mangold</p></td>
+            </tr>
+            <tr>
+              <td><h2>SCREENPLAY BY</h2></td>
+              <td><p>Michael Green and Scott Frank &amp; James Mangold</p></td>
+            </tr>
+            <tr>
+              <td><h2>STORY BY</h2></td>
+              <td><p>David James Kelly and James Mangold</p></td>
+            </tr>
+            <tr>
+              <td><h2>PRODUCED BY</h2></td>
+              <td><p>Hutch Parker, p.g.a., Simon Kinberg, p.g.a., Lauren Shuler Donner</p></td>
+            </tr>
+            <tr>
+              <td><h2>CASTS</h2></td>
+              <td><p>Hugh Jackman, Patrick Stewart, Richard E. Grant, Boyd Holbrook, Stephen Merchant, Introducing Dafne Keen</p></td>
+            </tr>
+          </table>
+        </div>
+
+         <br>
+        <img class="img-rating" src="images\imax.png" alt="imax logo">
+        <img class="img-rating" src="images\marvel.png" alt="marvel logo">
+        <img class="img-rating" src="images\rating.png" alt="rating">
+      </article>
+    </main>
+    <aside id="asideB">
+      <h1>Social Updates</h1>
+        <img src="images\Social_thumb.jpg" alt="SNS icon" class="img-circle" width="50px">
+      <h2>@LOGAN</h2>
+      <h3>03/14/2017 5:30PM</h3>
+      <p>Some want to see the beauty, but you should see the beast. #Logan is now playing in theaters. Get your tickets now:… <a href="https://t.co/IeQbYUNe8G" target="_blank">https://t.co/IeQbYUNe8G</a></p><br>
+      <a href="https://www.facebook.com/TheWolverineMovie?ref=br_tf" target="_blank"><img class="img-icon" src="images\facebook.png" alt="facebook icon">
+      <h4>View More on Facebook</h4></a><br>
+      <a href="https://www.twitter.com/wolverinemovie?ref=br_tf" target="_blank"><img class="img-icon" src="images\twitter.png" alt="twitter icon">
+      <h4>View More on Twitter</h4></a><br>
+      <a href="https://www.instagram.com/wponx" target="_blank"><img class="img-icon" src="images\instagram.png" alt="instagram icon">
+      <h4>View More on Instagram</h4></a>
+    </aside>
+
+    <footer>
+      <div id="terms">
+  				™ and © 2017 2017 Twentieth Century Fox Film Corporation. All rights reserved. Property of Fox. Use of this website (including any and all parts and components) constitutes your acceptance of these <a href="https://www.foxprivacy.com/us/terms.html" target="_blank">NEW Terms of Use</a> (<a href="https://www.foxprivacy.com/us/terms_whatsnew.html" target="_blank">What's new?</a>) and <a href="https://www.foxprivacy.com/us/privacy.html" target="_blank">NEW Privacy Policy</a>. <a href="https://www.foxprivacy.com/us/aboutourads.html" target="_blank">Ad Choices</a>. The materials on this website are not to be sold, traded or given away. Any copying, manipulation, publishing, or other transfer of these materials, except as specifically provided in the terms and conditions of use, is strictly prohibited. <a href="http://www.foxmovies.com/smoking-policy" target="_blank">Smoking Policy</a></div>
+    </footer>
+  </body>
+</html>
